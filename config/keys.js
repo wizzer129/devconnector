124ee880d11eda7c232334ed4beb26466d7a085e
Alejandro Lazo <alejandrolazo93@gmail.com>
2018-10-17 18:43:57 -0700

    First commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://alazo:Wizzer298!@ds135433.mlab.com:35433/swagconnector"
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello!"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running  on port ${port}`));
