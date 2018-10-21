0ef7bea2039dc226a15e53c064a5f82ed0957c3d
Alejandro Lazols <alejandrolazo93@gmail.com>
2018-10-20 20:38:01 -0700

    pushing api validation for APIs.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
 
 //Load User Model
 const User = require('../../models/User');
@@ -15,6 +21,12 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @desc register a user
 // @access Public
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email }).then(user => {
         if (user) {
             errors.email = 'Email already exists';
@@ -66,11 +78,45 @@ router.post('/login', (req, res) => {
         //Check Password
         bcrypt.compare(password, user.password).then(isMatch => {
             if (isMatch) {
-                res.json({ msg: 'Success' });
+                // User Matched
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    avatar: user.avatar
+                }; // Create JWT payload
+
+                //sign Token
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    { expiresIn: 3600 },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        });
+                    }
+                );
             } else {
                 return res.status(400).json({ password: 'Password Incorrect' });
             }
         });
     });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+    '/current',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        res.json({
+            id: req.user.id,
+            name: req.user.name,
+            email: req.user.email
+        });
+    }
+);
+
 module.exports = router;
