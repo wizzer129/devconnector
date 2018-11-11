52b92922865c319f78cf38583822f2f4fb3ef5a6
Alejandro Lazols <alejandrolazo93@gmail.com>
2018-11-10 21:25:53 -0800

    Added post comment and delete comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -186,4 +186,77 @@ router.post(
             );
     }
 );
+
+// @route POST api/post/comment/:id
+// @desc Add comment to post
+// @access Private
+router.post(
+    '/comment/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validatePostInput(req.body);
+
+        if (!isValid) {
+            // If any errors, send 400 with errors object
+            return res.status(400).json(errors);
+        }
+        Post.findById(req.params.id)
+            .then(post => {
+                const newComment = {
+                    text: req.body.text,
+                    name: req.body.name,
+                    avatar: req.body.avatar,
+                    user: req.user.id
+                };
+
+                // Add to comments to array
+                post.comments.unshift(newComment);
+                post.save().then(post => res.json(post));
+            })
+            .catch(err =>
+                res
+                    .status(404)
+                    .json({ nopostfound: 'No Post found with that ID' })
+            );
+    }
+);
+
+// @route DELETE api/post/comment/:id/:comment_id
+// @desc Remove Comment from post
+// @access Private
+router.delete(
+    '/comment/:id/:comment_id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Post.findById(req.params.id)
+            .then(post => {
+                // Check to see if commnet exists
+                if (
+                    post.comments.filter(
+                        comment =>
+                            comment._id.toString() === req.params.comment_id
+                    ).length === 0
+                ) {
+                    return res
+                        .status(404)
+                        .json({ commentnotexists: 'Comment does not exist' });
+                }
+
+                // Get remove index
+                const removeIndex = post.comments
+                    .map(item => item._id.toString())
+                    .indexOf(req.params.comment_id);
+
+                //Splice comment out of array
+                post.comments.splice(removeIndex, 1);
+
+                post.save().then(post => res.json(post));
+            })
+            .catch(err =>
+                res
+                    .status(404)
+                    .json({ nopostfound: 'No Post found with that ID' })
+            );
+    }
+);
 module.exports = router;
